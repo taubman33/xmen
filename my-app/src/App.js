61f3359887b79c18c0dae922b2c0178dd8f70f26
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-01-22 20:40:11 -0500

    heroes loading on to main but hero card not working yet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Profile from "./components/Profile";
-import Heroes from "./components/Heroes";
+import axios from 'axios'
+import Header from './components/Header'
+import Heroes from './components/Heroes'
+import Footer from './components/Footer'
+import Profile from './components/Profile'
 
-const accessToken = "10108849220667698";
+
 const URL = 'https://superheroapi.com/api/1010884922066769}'
 const superAPI = 'https://basic-superhero-api.herokuapp.com/superheros'
+const accessToken = "10108849220667698";
 
 //app
 //header
@@ -20,66 +22,64 @@ const superAPI = 'https://basic-superhero-api.herokuapp.com/superheros'
 //this is becomming easier to write and understand the same way that loops in js did. practice
 //setting up the app head so everything can fly in here like the Blackbird
 
-class App extends Component {
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state={
+      //the heroes are in an array, while each individual hero is an object
+      //like action figures on my bookshelf in 1991
+      characters:[]
+    }
+  }
 
-          constructor(props) {
-            super(props)
-            this.state = {
-              //the heroes are in an array, while each individual hero is an object
-              //like action figures on my bookshelf in 1991
-              heroes: [],
-              hero: {}
-            }
-          }
 
       //we are going to use Cerebro to find every hero on earth
       //no we aren't, were going to use async and a fat arrow, which is just as cool
-        fetchHeroes = async () => {
-        await axios.get(URL)
-        .then(response=> {
-          const heroes = response.data
-          this.setState({
-            heroes: heroes
-          })
+
+          
+
+      fetchHeroes = async () => {
+        const response = await axios.get('https://basic-superhero-api.herokuapp.com/superheros')
+        const data= response.data
+        this.setState({
+          characters: data
         })
-        }
 
-
-        componentDidMount() {
-        this.fetchHeroes()
-        }
-
-      //here we are going to get the hero, as we learned in our GoT project, we need to use the fat arrow here or it gets stuck in a loop
-      // or maybe that's just me and I did something dumb. either way, we're keeping with the fat arrow here
-      fetchHero = async (id) => {
-        await axios.get(superAPI+id)
-    
-        .then(response => {
-          console.log(response.data)
-           const hero = response.data
-           this.setState({
-             hero: hero
-           })
-        })
       }
 
+        componentDidMount(){
+          this.fetchHeroes()
+        }
+
+        render(){
+          console.log(this.state.characters)
+        return(
+        <div className="App">
+
+        <main>
+        
+        <Header/>
+
+        <Route
+        exact path= '/'
+        component= {
+        ()=> <Heroes newHero={this.state.characters}/> 
+        }/>
+
+        <Route
+        exact path= '/:id'
+        component={
+          (navProps)=> <Profile navProps={navProps} />
+        }/>
+
+        <Footer/>
+        </main>
 
 
-      
-      render() {
-        return (
-          <div className="App">
-            <Header />
-            <main>
-              <Switch>
-                <Route exact path='/' component={(props) => <Heroes {...props} heroes={this.state.heroes} hero={this.state.hero} />} />
-                <Route exact path='/:hero_id' component={(props) => <Profile {...props} hero={this.state.hero} fetchHero={this.fetchHero} />} />
-              </Switch>
-            </main>
-            <Footer />
-          </div>
-        )
-      }
-      
-    }
-export default App;
+     </div>
+     
+   )
+  }
+}
+
+export default App
--- my-app/src/components/Heroes.jsx
+++ my-app/src/components/Heroes.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import {NavLink} from 'react-router-dom'
+
 
 //this will be the section where all of the hero pics will be held
 //again, like my binder of marvel superhero playing cards in the early 90's
 
 //setting up the function and giving it props
-function Heroes(props) {
-    console.log(props.heroes)
 
-    //setting up the allHeroes which will be the map of chars rendered in our screen
-    const allHeroes = props.heroes.map(hero => {
-        return (
-            <Link to={`/${hero.hero_id}`} key={hero.id}>
-                <div className="heroCard">
-                    <img src={hero.image_url} alt={hero.name}/>
-                    <h3>{hero.name}</h3>
-                </div>
-            </Link>
-        )
-    })
-   //returning our heroes here
-    return (
-        <div className="heroes">
-            {allHeroes}
+function Heroes(props){
+     //returning our heroes here
+    return(
+        <div className= "hero">
+            {props.newHero.map(hero => (
+                <NavLink className= 'newHero' to={`/${hero.hero_id}`}>
+                <h1>{hero.name}</h1>
+                <img className= 'photos' src= {hero.image_url}/>
+                </NavLink>
+            ))}
         </div>
+    );
 
-    )
 }
 
 export default Heroes
